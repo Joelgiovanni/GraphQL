96e2bbfbfffe2711964ff93fdff15fa9d2a9b3ce
Joel Godoy <joelgiovanni4@gmail.com>
2020-09-14 12:07:25 -0600

    Main functions ready

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 import Launches from './components/Launches';
+import Launch from './components/Launch';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import logo from './img/06e498aa672e2293fb6bf509c54aba37.jpg';
 
@@ -12,20 +14,24 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className='container'>
-        <img
-          src={logo}
-          alt='space'
-          style={{
-            width: 300,
-            display: 'block',
-            margin: 'auto',
-            borderRadius: '100px',
-            marginTop: '80px',
-          }}
-        />
-        <Launches />
-      </div>
+      <Router>
+        <div className='container'>
+          <img
+            src={logo}
+            alt='space'
+            style={{
+              width: 300,
+              display: 'block',
+              margin: 'auto',
+              borderRadius: '100px',
+              marginTop: '80px',
+            }}
+          />
+          <Route exact path='/' component={Launches} />
+          <Route exact path='/launch/:flight_number' component={Launch} />
+          {/* <Launches /> */}
+        </div>
+      </Router>
     </ApolloProvider>
   );
 }
